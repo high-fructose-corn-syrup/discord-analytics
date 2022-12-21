17afbbad2645a87a7d6dfe7fe641cbdc1c835752
A. J <107040758+high-fructose-corn-syrup@users.noreply.github.com>
2022-12-21 07:16:26 -0600

    Update history.js

--- runners/history.js
+++ runners/history.js
@@ -24,7 +24,7 @@ async function exhaust_history(agent, parameters) {
         if (search_status == `OK`) {
             for (const message of search_results) {
                 if (!existsSync(`${PATH}/logs/master.csv`)) {
-                    writeFileSync(`${PATH}/logs/master.csv`, `message_snowflake,author_snowflake,user\n`)
+                    writeFileSync(`${PATH}/logs/master.csv`, `id,author_id,user\n`)
                 }
                 appendFileSync(`${PATH}/logs/master.csv`, `${message.id},${message.author.id},${message.author.username}\n`)
                 console.log(message.content)
